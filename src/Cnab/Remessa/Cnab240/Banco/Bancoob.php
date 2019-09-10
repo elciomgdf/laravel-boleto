e9a14ed60a353bfbb87f4f40277dded398fdea19
Elcio Guimarães <elcioguimaraes@gmail.com>
2019-09-09 22:11:30 -0300

    Teste

--- src/Cnab/Remessa/Cnab240/Banco/Bancoob.php
+++ src/Cnab/Remessa/Cnab240/Banco/Bancoob.php
@@ -308,7 +308,7 @@ class Bancoob extends AbstractRemessa implements RemessaContract
         $this->add(9, 9, 'R');
         $this->add(10, 11, '01');
         $this->add(12, 13, '');
-        $this->add(14, 16, '040');
+        $this->add(14, 16, '045');
         $this->add(17, 17, '');
         $this->add(18, 18, strlen(Util::onlyNumbers($this->getBeneficiario()->getDocumento())) == 14 ? 2 : 1);
         $this->add(19, 33, Util::formatCnab('9', Util::onlyNumbers($this->getBeneficiario()->getDocumento()), 15));
